8ba69df0cc0d2b5a0e08544d94dbfc1eb6192cbb
baggiiiie <yingchao.dai22@gmail.com>
2025-04-12 23:11:12 +0800

    feat: update blog url to notes.domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "baggiiiie.github.io/tech-stuff-blog",
+    baseUrl: "notes.yingchaoooo.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
