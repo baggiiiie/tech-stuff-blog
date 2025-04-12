972ae0900dac20616ca1d6c5fcdef4575fea06f8
baggiiiie <yingchao.dai22@gmail.com>
2025-04-12 23:18:15 +0800

    test: add trailing slash to see if it works?

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "notes.yingchaoooo.com",
+    baseUrl: "notes.yingchaoooo.com/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
